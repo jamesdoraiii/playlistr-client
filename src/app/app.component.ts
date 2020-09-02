ae0897a1bfcf2d6de9e607612ab2bee61324b313
James Dora <jamesdoraiii@gmail.com>
2020-09-02 16:01:02 -0500

    working on sidebar, added embedded web player as a placeholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.checkWidth();
+  }
   title = 'angular-tailwind-starter';
   isMenuOpen: boolean;
+
+  ngOnInit() {
+    this.checkWidth();
+  }
+
+  checkWidth() {
+    if (window.innerWidth > 767) {
+      this.isMenuOpen = true;
+    }
+  }
 }
