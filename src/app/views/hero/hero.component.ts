930f2dd4fd0ff2f32cd504498882cc0a8e085ed5
James Dora <jamesdoraiii@gmail.com>
2020-09-28 20:04:39 -0500

    working on issue with routing to playlist detail

--- src/app/views/hero/hero.component.ts
+++ src/app/views/hero/hero.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  getStarted() {
+    this.router.navigate(['/home']);
   }
-
 }
