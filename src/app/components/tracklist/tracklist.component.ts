eeef65fdaccbad56e34858b9ee6988d2c76c24c2
James Dora <jamesdoraiii@gmail.com>
2021-02-07 14:50:11 -0600

    Spotify player sdk is working! You can now click tracks and actually play them through the browser!

--- src/app/components/tracklist/tracklist.component.ts
+++ src/app/components/tracklist/tracklist.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+
+import { WebPlayerService } from '@services/web-player.service';
 @Component({
   selector: 'app-tracklist',
   templateUrl: './tracklist.component.html',
@@ -7,9 +9,13 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TracklistComponent implements OnInit {
   @Input() tracks: any;
 
-  constructor() {}
+  constructor(private webPlayerService: WebPlayerService) {}
 
   ngOnInit(): void {
     console.log(this.tracks);
+  }
+
+  playTrack(uri: string) {
+    this.webPlayerService.playTrack(uri);
   }
 }
--- src/app/services/web-player.service.ts
+++ src/app/services/web-player.service.ts
@@ -5,6 +5,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WebPlayerService {
+  player: any;
+  token =
+    'BQDWjxgWuBRXbkRmM2pS-_xdPpGlGRuD9EUFnaMLhS4VjEtuIybY98qmCXXrkr6J0KYd_mgLIRrB1PtTzPF5CKtbhRrKQIEkc0sMMUw5z5xm934_MuDKlAj48SDHcQkqJsCRNZ-HPVw3xRYjIPw9OhFV2TmbHtEmEw';
   get window(): any {
     return window;
   }
@@ -13,47 +16,69 @@ export class WebPlayerService {
 
   initializeSpotifyWebPlayer() {
     this.window.onSpotifyWebPlaybackSDKReady = () => {
-      const token =
-        'BQBRo3mxbYvPQ7Z6vTJfIOGhIQo7z_zZBLlXF1g_mHuqQK9xTlSDflXrAmrTWoGFpr4t2NRX5Z4Jn3FmBQGywE2N7nitXW6KDCaD0yB2CjERkCWhSVG-vdmEdkdf8iCw_Fvpg2KqZ61kzW27b6i_w1ctWy2t_xq_eQ';
-
-      const player = new Spotify.Player({
+      this.player = new Spotify.Player({
         name: 'Web Playback SDK Quick Start Player',
         getOAuthToken: cb => {
-          cb(token);
+          cb(this.token);
         }
       });
 
       // Error handling
-      player.addListener('initialization_error', ({ message }) => {
+      this.player.addListener('initialization_error', ({ message }) => {
         console.error(message);
       });
-      player.addListener('authentication_error', ({ message }) => {
+      this.player.addListener('authentication_error', ({ message }) => {
         console.error(message);
       });
-      player.addListener('account_error', ({ message }) => {
+      this.player.addListener('account_error', ({ message }) => {
         console.error(message);
       });
-      player.addListener('playback_error', ({ message }) => {
+      this.player.addListener('playback_error', ({ message }) => {
         console.error(message);
       });
 
       // Playback status updates
-      player.addListener('player_state_changed', state => {
+      this.player.addListener('this.player_state_changed', state => {
         console.log(state);
       });
 
       // Ready
-      player.addListener('ready', ({ device_id }) => {
+      this.player.addListener('ready', ({ device_id }) => {
         console.log('Ready with Device ID', device_id);
       });
 
       // Not Ready
-      player.addListener('not_ready', ({ device_id }) => {
+      this.player.addListener('not_ready', ({ device_id }) => {
         console.log('Device ID has gone offline', device_id);
       });
 
-      // Connect to the player!
-      player.connect();
+      // Connect to the this.player!
+      this.player.connect();
     };
+  }
+
+  playRequest = ({
+    spotify_uri,
+    playerInstance: {
+      _options: { getOAuthToken, id }
+    }
+  }) => {
+    getOAuthToken(access_token => {
+      fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ uris: [spotify_uri] }),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${access_token}`
+        }
+      });
+    });
+  };
+
+  playTrack(trackUri: string) {
+    this.playRequest({
+      spotify_uri: trackUri,
+      playerInstance: this.player
+    });
   }
 }
