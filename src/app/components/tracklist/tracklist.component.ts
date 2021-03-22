a717fdebe9e9048dccd1a9da0bada153b581d3de
James Dora <jamesdoraiii@gmail.com>
2021-03-22 17:59:31 -0500

    setup proper track duration display

--- src/app/components/tracklist/tracklist.component.ts
+++ src/app/components/tracklist/tracklist.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 import { WebPlayerService } from '@services/web-player.service';
+
 @Component({
   selector: 'app-tracklist',
   templateUrl: './tracklist.component.html',
@@ -17,5 +18,10 @@ export class TracklistComponent implements OnInit {
 
   playTrack(uri: string) {
     this.webPlayerService.playTrack(uri);
+  }
+  convertmillisToMinutesAndSeconds(millis) {
+    const minutes = Math.floor(millis / 60000);
+    const seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ':' + (parseInt(seconds) < 10 ? '0' : '') + seconds;
   }
 }
