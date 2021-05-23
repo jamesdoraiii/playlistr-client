079e2fafcf22a672e542c7c5790d8b862139020e
James Dora <jamesdoraiii@gmail.com>
2021-05-23 17:14:57 -0500

    setup node vibrant to color backgrounds based on album artwork, working on setting up a generic item detail component

--- src/app/components/item-header/item-header.component.ts
+++ src/app/components/item-header/item-header.component.ts
@@ -1,16 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+
+const Vibrant = require('node-vibrant');
 
 @Component({
   selector: 'app-item-header',
   templateUrl: './item-header.component.html',
   styleUrls: ['./item-header.component.css']
 })
-export class ItemHeaderComponent implements OnInit {
+export class ItemHeaderComponent implements OnChanges {
   @Input() item: any;
+  palette: any;
 
   constructor() {}
 
-  ngOnInit(): void {
-    console.log(this.item);
+  ngOnChanges(): void {
+    this.getVibrantColor(this.item.images[0].url);
+  }
+
+  getVibrantColor(url: string) {
+    Vibrant.from(url).getPalette((err, palette) => {
+      this.palette = palette;
+    });
+  }
+
+  styleContainer(): any {
+    if (this.palette && this.palette.Vibrant) {
+      return {
+        background: 'linear-gradient(transparent 0,rgba(0,0,0,.5) 100%),' + this.palette.Vibrant.getHex()
+      };
+    } else {
+      return {};
+    }
   }
 }
--- src/app/views/item-detail/item-detail.component.ts
+++ src/app/views/item-detail/item-detail.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+
+const Vibrant = require('node-vibrant');
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
   styleUrls: ['./item-detail.component.css']
 })
-export class ItemDetailComponent implements OnInit {
+export class ItemDetailComponent implements OnChanges {
+  @Input() item: any;
+  palette: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.getVibrantColor(this.item.images[0].url);
   }
 
+  getVibrantColor(url: string) {
+    Vibrant.from(url).getPalette((err, palette) => {
+      this.palette = palette;
+    });
+  }
+
+  styleContainer(): any {
+    if (this.palette && this.palette.Vibrant) {
+      return {
+        background: 'linear-gradient(transparent 0,rgba(0,0,0,.5) 100%),' + this.palette.Vibrant.getHex()
+      };
+    } else {
+      return {};
+    }
+  }
 }
