14cf3b0937720bfa39d838f59b04fa03e515499d
James Dora <jamesdoraiii@gmail.com>
2021-07-09 11:43:58 -0500

    tags are now being submitted properly with playlist posts

--- src/app/services/playlists.service.ts
+++ src/app/services/playlists.service.ts
@@ -101,9 +101,9 @@ export class PlaylistsService {
     return this.apollo.mutate({
       mutation: gql`
         mutation CreatePlaylist {
-          createPlaylist(input: {playlist: {ownerId: 7, spotifyPlaylistId: "${spotifyPlaylistId}", genreTags: "${genreTags.join(
-        ', '
-      )}", isValid: false, name: "Test Playlist", ownerUsername: "jimmyd233", imageUrl: "https://d29rinwu2hi5i3.cloudfront.net/article_media/78a90e8c-b043-426f-a87e-f79d6de6c62e/cdj-2000nexus-screen_copy.jpg"}}) {
+          createPlaylist(input: {playlist: {ownerId: 7, spotifyPlaylistId: "${spotifyPlaylistId}", genreTags: ${JSON.stringify(
+        genreTags
+      )}, isValid: false, name: "Test Playlist", ownerUsername: "jimmyd233", imageUrl: "https://d29rinwu2hi5i3.cloudfront.net/article_media/78a90e8c-b043-426f-a87e-f79d6de6c62e/cdj-2000nexus-screen_copy.jpg"}}) {
             clientMutationId
           }
         }
