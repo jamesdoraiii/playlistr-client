53ea699c074faaab6af090d6dc779812c7881a64
James Dora <jamesdoraiii@gmail.com>
2021-08-09 17:05:16 -0500

    setup basic methods for creating updating and deleting votes in the voting service

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { PlaylistsService } from '@services/playlists.service';
 import { WebPlayerService } from '@services/web-player.service';
@@ -10,6 +10,7 @@ import { WebPlayerService } from '@services/web-player.service';
 })
 export class SidebarComponent implements OnInit {
   nowPlayingImageUrl: string;
+  @Output() closeMenu = new EventEmitter<any>();
 
   get userPlaylists(): any[] {
     return this.playlistsService.userPlaylists;
--- src/app/services/vote.service.ts
+++ src/app/services/vote.service.ts
@@ -1,9 +1,58 @@
+import { Apollo } from 'apollo-angular';
+import { AuthService } from '@services/auth.service';
+import { BaseService } from '@services/base.service';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import gql from 'graphql-tag';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoteService {
+  constructor(
+    protected http: HttpClient,
+    private base: BaseService,
+    private apollo: Apollo,
+    private auth: AuthService
+  ) {}
 
-  constructor() { }
+  createVote(ownerId: string, parentPlaylistId: string, isUpvote: boolean) {
+    return this.apollo.mutate({
+      mutation: gql`
+        mutation MyMutation {
+          createVote(input: { vote: { ownerId: ${this.auth.userInfo.userId}, isUpvote: ${isUpvote}, parentPlaylistId: ${parentPlaylistId} } }) {
+            vote {
+              nodeId
+              createdAt
+              isUpvote
+              voteId
+            }
+          }
+        }
+      `
+    });
+  }
+
+  deleteVote(voteId: string) {
+    return this.apollo.mutate({
+      mutation: gql`mutation MyMutation {
+        deleteVoteByVoteId(input: {voteId: ${voteId}}) {
+          vote {
+            voteId
+          }
+        }
+      }`
+    });
+  }
+
+  updateVote(voteId: string, isUpvote: boolean) {
+    return this.apollo.mutate({
+      mutation: gql`mutation MyMutation {
+        updateVoteByVoteId(input: {votePatch: {isUpvote: ${isUpvote}}, voteId: ${voteId}}) {
+          clientMutationId
+        }
+      }
+      `
+    });
+  }
 }
